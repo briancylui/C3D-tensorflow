a71aa03392dff28de04bd737cdb3b5efd53525b3
Brian Lui <briancylui@gmail.com>
2019-05-16 20:57:20 -0700

    Changed feature saving

--- save_video_features.py
+++ save_video_features.py
@@ -15,6 +15,7 @@
 
 
 import os
+import sys
 import tensorflow as tf
 import PIL.Image as Image
 import random
@@ -31,7 +32,7 @@ GPU_NUM = 4
 MODEL_NAME = './models/c3d_ucf101_finetune_whole_iter_20000_TF.model'
 model = c3d_model_ucfcrime
 FEATURE_FILE = './ucfcrime_c3d_features.h5'
-LISTS = ['./list/test.list', './list/train.list']
+LISTS = ['./list/test.list']
 
 
 def get_random_crop(image_np, crop_size=CROP_SIZE):
@@ -67,8 +68,8 @@ def get_image_batch_for_segment(video_path, frames_list, num_frames_per_clip=NUM
             image = Image.open(os.path.join(video_path, frames_list[frame_index]))
             image_np = np.array(image) # image.size (W, H) -> image_data.shape (H, W, C)
             # Randomly crop image_np (H, W, C) into (crop_size, crop_size, C)
-            image_cropped = get_random_crop(image_np)
-            clip.append(np.expand_dims(image_cropped, axis=0)) # (H, W, C) -> (1, H, W, C)
+            # image_cropped = get_random_crop(image_np)
+            clip.append(np.expand_dims(image_np, axis=0)) # (H, W, C) -> (1, H, W, C)
         clip = np.concatenate(clip) # (F, H, W, C)
         # Optional: Horizontally flip the entire clip w.p. 50% by uncommenting:
         # if np.random.random() < 0.5: clip = np.flip(clip, axis=2)
@@ -82,35 +83,35 @@ def get_segment_features(video_path, frames_list, num_frames_per_clip=NUM_FRAMES
     clip_features = []
     num_frames = len(frames_list)
     if num_frames < num_frames_per_clip:
-        return
+        return None
     
     batch = get_image_batch_for_segment(video_path, frames_list, NUM_FRAMES_PER_CLIP, \
         CLIP_INCREMENT)
 
     images_placeholder = tf.placeholder(tf.float32, shape=batch.shape)
     with tf.variable_scope('var_name') as var_scope:
-    weights = {
-            'wc1': _variable_with_weight_decay('wc1', [3, 3, 3, 3, 64], 0.04, 0.00),
-            'wc2': _variable_with_weight_decay('wc2', [3, 3, 3, 64, 128], 0.04, 0.00),
-            'wc3a': _variable_with_weight_decay('wc3a', [3, 3, 3, 128, 256], 0.04, 0.00),
-            'wc3b': _variable_with_weight_decay('wc3b', [3, 3, 3, 256, 256], 0.04, 0.00),
-            'wc4a': _variable_with_weight_decay('wc4a', [3, 3, 3, 256, 512], 0.04, 0.00),
-            'wc4b': _variable_with_weight_decay('wc4b', [3, 3, 3, 512, 512], 0.04, 0.00),
-            'wc5a': _variable_with_weight_decay('wc5a', [3, 3, 3, 512, 512], 0.04, 0.00),
-            'wc5b': _variable_with_weight_decay('wc5b', [3, 3, 3, 512, 512], 0.04, 0.00),
-            'wd1': _variable_with_weight_decay('wd1', [8192, 4096], 0.04, 0.001)
-            }
-    biases = {
-            'bc1': _variable_with_weight_decay('bc1', [64], 0.04, 0.0),
-            'bc2': _variable_with_weight_decay('bc2', [128], 0.04, 0.0),
-            'bc3a': _variable_with_weight_decay('bc3a', [256], 0.04, 0.0),
-            'bc3b': _variable_with_weight_decay('bc3b', [256], 0.04, 0.0),
-            'bc4a': _variable_with_weight_decay('bc4a', [512], 0.04, 0.0),
-            'bc4b': _variable_with_weight_decay('bc4b', [512], 0.04, 0.0),
-            'bc5a': _variable_with_weight_decay('bc5a', [512], 0.04, 0.0),
-            'bc5b': _variable_with_weight_decay('bc5b', [512], 0.04, 0.0),
-            'bd1': _variable_with_weight_decay('bd1', [4096], 0.04, 0.0)
-            }
+        weights = {
+                'wc1': _variable_with_weight_decay('wc1', [3, 3, 3, 3, 64], 0.04, 0.00),
+                'wc2': _variable_with_weight_decay('wc2', [3, 3, 3, 64, 128], 0.04, 0.00),
+                'wc3a': _variable_with_weight_decay('wc3a', [3, 3, 3, 128, 256], 0.04, 0.00),
+                'wc3b': _variable_with_weight_decay('wc3b', [3, 3, 3, 256, 256], 0.04, 0.00),
+                'wc4a': _variable_with_weight_decay('wc4a', [3, 3, 3, 256, 512], 0.04, 0.00),
+                'wc4b': _variable_with_weight_decay('wc4b', [3, 3, 3, 512, 512], 0.04, 0.00),
+                'wc5a': _variable_with_weight_decay('wc5a', [3, 3, 3, 512, 512], 0.04, 0.00),
+                'wc5b': _variable_with_weight_decay('wc5b', [3, 3, 3, 512, 512], 0.04, 0.00),
+                'wd1': _variable_with_weight_decay('wd1', [8192, 4096], 0.04, 0.001)
+                }
+        biases = {
+                'bc1': _variable_with_weight_decay('bc1', [64], 0.04, 0.0),
+                'bc2': _variable_with_weight_decay('bc2', [128], 0.04, 0.0),
+                'bc3a': _variable_with_weight_decay('bc3a', [256], 0.04, 0.0),
+                'bc3b': _variable_with_weight_decay('bc3b', [256], 0.04, 0.0),
+                'bc4a': _variable_with_weight_decay('bc4a', [512], 0.04, 0.0),
+                'bc4b': _variable_with_weight_decay('bc4b', [512], 0.04, 0.0),
+                'bc5a': _variable_with_weight_decay('bc5a', [512], 0.04, 0.0),
+                'bc5b': _variable_with_weight_decay('bc5b', [512], 0.04, 0.0),
+                'bd1': _variable_with_weight_decay('bd1', [4096], 0.04, 0.0)
+                }
     
     features = []
     batch_size = batch.shape[0] / GPU_NUM
@@ -123,7 +124,7 @@ def get_segment_features(video_path, frames_list, num_frames_per_clip=NUM_FRAMES
             else:
                 feature = model.inference_c3d(images_placeholder[gpu_index * \
                     batch_size:,:,:,:,:], 0.6, batch_size, weights, biases)
-        features.append(feature) # (B / GPU_NUM, 4096)
+            features.append(feature) # (B / GPU_NUM, 4096)
     features = tf.concat(features, axis=0) # (B, 4096)
     features = tf.reduce_mean(features, axis=0) # (4096,)
 
@@ -143,27 +144,30 @@ def get_segment_features(video_path, frames_list, num_frames_per_clip=NUM_FRAMES
     return segment_features
 
 
-def save_video_features(filename, num_segments_per_video=NUM_SEGMENTS_PER_VIDEO):
+def save_video_features(filename, num_segments_per_video=NUM_SEGMENTS_PER_VIDEO, start_index=0, end_index=340):
     lines = open(filename, 'r').readlines()
-    for video_index in tqdm(range(len(lines))):
+    tf.random.set_random_seed(SEED)
+
+    for video_index in tqdm(range(start_index, end_index)):
         line = lines[video_index].strip('\n').split()
         dirname = line[0]
         resized_dirname = dirname + '_resized'
-        for video, _, frames in os.walk(resized_dirname):
-            num_frames = len(frames)
-            num_frames_per_segment = num_frames / num_segments_per_video
-            video_features = []
-            for segment_index in range(num_segments_per_video):
-                frames_list = frames[num_frames_per_segment * \
-                    segment_index:num_frames_per_segment * (segment_index + 1)]
-                segment_features = get_segment_features(resized_dirname, frames_list)
+
+        frames = os.listdir(resized_dirname)
+        num_frames = len(frames)
+        num_frames_per_segment = num_frames / num_segments_per_video
+        video_features = []
+        for segment_index in range(num_segments_per_video):
+            frames_list = frames[num_frames_per_segment * \
+                segment_index:num_frames_per_segment * (segment_index + 1)]
+            segment_features = get_segment_features(resized_dirname, frames_list)
+            if segment_features is not None:
                 video_features.append(np.expand_dims(segment_features, axis=0)) # (1, 4096)
-            video_features = np.concatenate(video_features) # (NUM_SEGMENTS_PER_VIDEO, 4096)
-            with h5py.File(FEATURE_FILE, 'w') as hf:
-                hf.create_dataset(resized_dirname, data=video_features)
-            break
+        video_features = np.concatenate(video_features) # (NUM_SEGMENTS_PER_VIDEO, 4096)
+        with h5py.File(FEATURE_FILE, 'w') as hf:
+            hf.create_dataset(resized_dirname, data=video_features)
 
 if __name__ = '__main__':
     for listname in LISTS:
-        save_video_features(listname)
+        save_video_features(listname, start_index=int(sys.argv[1]), end_index=int(sys.argv[2]))
         print('Done saving features for videos in {}'.format(listname))
