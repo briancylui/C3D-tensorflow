7f93b54ae2b4831a1ae6390070838fe46db3c391
Brian Lui <briancylui@gmail.com>
2019-05-17 05:28:52 -0700

    Fixed syntax error

--- save_video_features.py
+++ save_video_features.py
@@ -125,7 +125,7 @@ def get_segment_features(video_path, frames_list, num_frames_per_clip=NUM_FRAMES
     if batch_size == 0:
         with tf.device('/gpu:0'):
             batch = images_placeholder
-            
+
             cropped = tf.image.random_crop(batch, [batch.shape[0], NUM_FRAMES_PER_CLIP, CROP_SIZE, CROP_SIZE, CHANNELS])
             cropped_zero_mean = tf.subtract(cropped, mean_placeholder)
             feature = model.inference_c3d(cropped_zero_mean, 0.6, batch.shape[0], weights, biases)
@@ -135,7 +135,7 @@ def get_segment_features(video_path, frames_list, num_frames_per_clip=NUM_FRAMES
             with tf.device('/gpu:%d' % gpu_index):
                 if gpu_index != GPU_NUM - 1:
                     batch = images_placeholder[gpu_index * batch_size:(gpu_index + 1) * batch_size,:,:,:,:]
-                else
+                else:
                     batch = images_placeholder[gpu_index * batch_size:,:,:,:,:]
                 
                 cropped = tf.image.random_crop(batch, [batch.shape[0], NUM_FRAMES_PER_CLIP, CROP_SIZE, CROP_SIZE, CHANNELS])
