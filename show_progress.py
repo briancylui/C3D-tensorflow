f8323bedcfd0d9da966fe1fb2de5eac1842b8f19
Brian Lui <briancylui@gmail.com>
2019-05-19 03:07:17 -0700

    Added helper file to print progress on stdout

---
+++ show_progress.py
@@ -0,0 +1,12 @@
+import subprocess
+
+queue_command = "squeue | grep brianlui"
+output = subprocess.check_output(queue_command.split(), stdout=subprocess.PIPE)
+print(output)
+
+lines = output.strip().splitlines()
+for line in lines:
+    job_id = line.split()[0]
+    tail_command = "tail -1 save-" + job_id + ".out"
+    output = subprocess.check_output(tail_command.split(), stdout=subprocess.PIPE)
+    print(output)
