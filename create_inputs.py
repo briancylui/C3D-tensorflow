9f3390cfce3ca19ab9f9b199b7196ef2076b674e
Brian Lui <briancylui@gmail.com>
2019-05-22 07:43:01 -0700

    Modified create_inputs.py

--- create_inputs.py
+++ create_inputs.py
@@ -1,13 +1,25 @@
 import os
 import numpy as np
+from tqdm import tqdm
 
 feature_list = []
 with open('./list/train.list', 'r') as f:
     lines = f.readlines()
-    for line in lines:
+    for line in tqdm(lines):
         dirname = line.split()[0]
         video_features = np.load(dirname + '.npy')
         feature_list.append(video_features)
 
 features = np.concatenate(feature_list)
-np.save('features.npy')
+np.save('train_features.npy')
+
+feature_list = []
+with open('./list/test.list', 'r') as f:
+    lines = f.readlines()
+    for line in tqdm(lines):
+        dirname = line.split()[0]
+        video_features = np.load(dirname + '.npy')
+        feature_list.append(video_features)
+
+features = np.concatenate(feature_list)
+np.save('test_features.npy')
